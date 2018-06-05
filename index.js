37f27642dfd9d4805ab8826abfc8d7b9f3b886f3
c <ytmssr@outlook.com>
2018-06-05 22:11:44 +0800

    hello button

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Button from '@material-ui/core/Button';
+
 const App = (props) => {
     return (
-        <h1>Hello</h1>
+        <div>
+            <Button variant="raised" color="primary">
+                Hello World
+            </Button>
+        </div>
     )
 }
 
