59ebc515ceac10a03500b37b3941b52a7b4468fc
c <ytmssr@outlook.com>
2018-06-05 22:02:27 +0800

    new file:   index.html 	new file:   index.js 	new file:   package.json 	.babelrc 	.gitignore

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (props) => {
+    return (
+        <h1>Hello</h1>
+    )
+}
+
+const root = document.getElementById('root')
+
+ReactDOM.render(<App/>, root)
